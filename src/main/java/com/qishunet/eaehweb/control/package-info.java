cfe8f56753ec57e6fcaa8ae94eb4368a04177e4d
wangzh <hn_wangzh@163.com>
2016-11-29 11:41:27 +0800

    Revert "创建"
    This reverts commit 24ba7a39955b9204492c491fdf554e79bb323317.

--- src/main/java/com/qishunet/eaehweb/control/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * @author qishu008
- *
- */
-package com.qishunet.eaehweb.control;
--- src/main/java/com/qishunet/eaehweb/framework/service/ServiceLocatorFactory.java
+++
@@ -1,8 +0,0 @@
-package com.qishunet.eaehweb.framework.service;
-
-
-public class ServiceLocatorFactory {
-	public static ServiceLocator getServiceLocator(){
-		return ServiceLocator.getInstance();
-	}
-}
